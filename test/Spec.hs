a703455c9a476a3fcb8b077df83970afdb67d4ee
Petr Prokhorenkov <eeight@yandex-team.ru>
2015-11-08 16:56:45 +0300

    Add complex structure test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -25,33 +25,54 @@ instance FromMms Point where
     mmsSize _ = 16
     readFields = liftM2 Point readFields readFields
 
-newtype AllocatedVector a = AllocatedVector
-    { unAllocated :: [a]
-    } deriving (Foldable)
+data Mode = Allocated | Mapped
 
-data MappedVector a = MappedVector (Ptr a) Int64
+data Vector (m :: Mode) a where
+    AllocatedVector :: { unAllocated :: [a] } -> Vector 'Allocated a
+    MappedVector :: Ptr a -> Int64 -> Vector 'Mapped a
 
-toList :: FromMms a => MappedVector a -> [a]
+toList :: FromMms a => Vector 'Mapped a -> [a]
 toList (MappedVector p length) = let
     xs = take (fromIntegral length) $ map readMms $
         iterate (`plusPtr` elementSize) p
     elementSize = mmsSize $ head xs
     in xs
 
-instance ToMms a => ToMms (AllocatedVector a) where
-    type Freeze (AllocatedVector a) = MappedVector (Freeze a)
+instance (Show a, FromMms a) => Show (Vector m a) where
+    show (AllocatedVector xs) = show xs
+    show v@MappedVector{} = show . toList $ v
+
+instance Foldable (Vector 'Allocated) where
+    foldr f z (AllocatedVector xs) = foldr f z xs
+
+instance ToMms a => ToMms (Vector 'Allocated a) where
+    type Freeze (Vector 'Allocated a) = Vector 'Mapped (Freeze a)
 
     writeData xs = mapM_ writeData xs >> saveOffset >> mapM_ writeFields xs
     writeFields xs = do
         writeOffset =<< loadOffset
         putStorable $ (fromIntegral (length xs) :: Int64)
 
-instance FromMms a => FromMms (MappedVector a) where
+instance FromMms a => FromMms (Vector 'Mapped a) where
     mmsSize _ = 16
     readFields = liftM2 MappedVector getPointer getStorable
 
 instance Arbitrary Point where
     arbitrary = liftM2 Point arbitrary arbitrary
+
+data SomeData m = SomeData
+    { points :: Vector m Point
+    , number :: Double
+    } deriving (Show)
+
+instance ToMms (SomeData 'Allocated) where
+    type Freeze (SomeData 'Allocated) = SomeData 'Mapped
+    writeData SomeData{..} = writeData points >> writeData number
+    writeFields SomeData{..} = writeFields points >> writeFields number
+
+instance FromMms (SomeData 'Mapped) where
+    mmsSize ~SomeData{..} = mmsSize points + mmsSize number
+    readFields = liftM2 SomeData readFields readFields
 
 main :: IO ()
 main = hspec $ do
@@ -65,7 +86,7 @@ main = hspec $ do
             L.length (writeMms p) `shouldBe` 16
 
         it "Vector of two doubles takes 32 bytes" $ do
-            let xs = AllocatedVector [1.8, 2.4] :: AllocatedVector Double
+            let xs = AllocatedVector [1.8, 2.4 :: Double]
             L.length (writeMms xs) `shouldBe` 32
 
     describe "readFields . toStrict . writeFields = id" $ do
@@ -80,10 +101,15 @@ main = hspec $ do
     describe "Write variable-length data" $ do
         it "Two-component vector can be retrieved back" $ do
             let xs = AllocatedVector [1.8, 2.4 :: Double]
-            let xs' = readMms (L.toStrict . writeMms $ xs) :: MappedVector Double
+            let xs' = readMms . L.toStrict . writeMms $ xs :: Vector 'Mapped Double
             toList xs' `shouldBe` unAllocated xs
 
-        it "Same for a matrix " $ do
+        it "Same for a matrix" $ do
             let xs = AllocatedVector (map AllocatedVector [[1, 2], [3, 4 :: Double]])
-            let xs' = readMms (L.toStrict . writeMms $ xs) :: MappedVector (MappedVector Double)
+            let xs' = readMms (L.toStrict . writeMms $ xs) :: Vector 'Mapped (Vector 'Mapped Double)
             map toList (toList xs') `shouldBe` map unAllocated (unAllocated xs)
+
+        it "Same for SomeData" $ do
+            let ed = SomeData (AllocatedVector [Point 1 2, Point 3 4]) 18 :: SomeData 'Allocated
+            let ed' = readMms . L.toStrict . writeMms $ ed :: SomeData 'Mapped
+            show ed' `shouldBe` show ed
